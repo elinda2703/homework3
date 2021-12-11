348406b1ce48de1602ff220cfa90e5b88578b12c
elinda <ela.pospechova@gmail.com>
2021-12-11 23:24:33 +0100

    přidány věty, které program vypíše, vzdálenosti přvedeny na integer, protože to vypadá líp

--- du3.py
+++ du3.py
@@ -3,51 +3,48 @@ from pyproj import Transformer
 from math import sqrt
 import statistics
 import sys
+try:
+    with open("adresy.geojson", encoding="utf-8") as a:
+        adresy = json.load(a)
 
-with open("adresy.geojson", encoding="utf-8") as a:
-    adresy=json.load(a)
+    with open("kontejnery.geojson", encoding="utf-8") as k:
+        kontejnery = json.load(k)
 
-with open("kontejnery.geojson", encoding="utf-8") as k:
-    kontejnery=json.load(k)
+    wgs2jtsk = Transformer.from_crs(4326, 5514)
+    vzdalenosti_min = {}
+    vzdalenost = None
+    for i in range(len(adresy['features'])):
+        ulice_cislo = f"{adresy['features'][i]['properties']['addr:street']} {adresy['features'][i]['properties']['addr:housenumber']}"
+        adresa_y, adresa_x = adresy['features'][i]['geometry']['coordinates']
+        souradnice_krovak = wgs2jtsk.transform(adresa_x, adresa_y)
+        vzdalenosti_min[ulice_cislo] = None
+        for j in range(len(kontejnery['features'])):
+            adresa_kontejneru = str(
+                kontejnery['features'][j]['properties']['STATIONNAME'])
+            kontejner_x, kontejner_y = kontejnery['features'][j]['geometry']['coordinates']
+            vzdalenost = int(round(sqrt(
+                (souradnice_krovak[0]-kontejner_x)**2+(souradnice_krovak[1]-kontejner_y)**2), 0))
+            if kontejnery['features'][j]['properties']['PRISTUP'] == "obyvatelům domu" and ulice_cislo == adresa_kontejneru:
+                vzdalenosti_min[ulice_cislo] = 0
+            elif kontejnery['features'][j]['properties']['PRISTUP'] == "volně":
+                if vzdalenosti_min[ulice_cislo] == None or vzdalenosti_min[ulice_cislo] > vzdalenost:
+                    vzdalenosti_min[ulice_cislo] = vzdalenost
+        if vzdalenosti_min[ulice_cislo] > 10000:
+            print("příliš velká vzdálenost k nejblišímu kontejneru")
+            sys.exit()
 
-wgs2jtsk = Transformer.from_crs(4326,5514)
-vzdalenosti_min={}
-vzdalenost=None
+    maximum = 0
+    adresa_maxima = None
+    median = statistics.median(vzdalenosti_min.values())
+    prumer = int(round(statistics.mean(vzdalenosti_min.values()), 0))
+    for a, d in vzdalenosti_min.items():
+        if maximum < d:
+            maximum = d
+            adresa_maxima = str(a)
 
-
-for i in range (len(adresy['features'])):
-    ulice_cislo=f"{adresy['features'][i]['properties']['addr:street']} {adresy['features'][i]['properties']['addr:housenumber']}"
-    adresa_x=adresy['features'][i]['geometry']['coordinates'][1]
-    adresa_y=adresy['features'][i]['geometry']['coordinates'][0]
-    souradnice_krovak=wgs2jtsk.transform(adresa_x,adresa_y)
-    vzdalenosti_min[ulice_cislo]=None
-    for j in range (len(kontejnery['features'])):
-        adresa_kontejneru=str(kontejnery['features'][j]['properties']['STATIONNAME'])
-        kontejner_x,kontejner_y=kontejnery['features'][j]['geometry']['coordinates']
-        vzdalenost=sqrt((souradnice_krovak[0]-kontejner_x)**2+(souradnice_krovak[1]-kontejner_y)**2)
-        if kontejnery['features'][j]['properties']['PRISTUP']=="obyvatelům domu" and ulice_cislo==adresa_kontejneru:
-            vzdalenosti_min[ulice_cislo]=0
-        elif kontejnery['features'][j]['properties']['PRISTUP']=="volně":
-            if vzdalenosti_min[ulice_cislo]==None or vzdalenosti_min[ulice_cislo]>vzdalenost:
-                vzdalenosti_min[ulice_cislo]=vzdalenost
-    if vzdalenosti_min[ulice_cislo]>10000:
-        print("příliš velká vzdálenost k nejblišímu kontejneru")
-        sys.exit()
-
-     
-
-maximum=0
-maximum_s_adresou={}
-median=statistics.median(vzdalenosti_min.values())
-prumer=statistics.mean(vzdalenosti_min.values())
-for a,d in vzdalenosti_min.items():
-    if maximum<d:
-        maximum=d
-        maximum_s_adresou={a:d}
-
-
-print(prumer)
-print(maximum_s_adresou)
-print(median)
-print(i,j)
-
+    print(f"Průměrná vzdálenost k nejbližšímu kontejneru je {prumer} m.")
+    print(f"Nejdále je to ke kontejneru z adresy {adresa_maxima} a to {maximum} m.")
+    print(f"Medián vzdáleností je {median} m.")
+    print(i, j)
+except FileNotFoundError:
+    print("chybná vstupní data")
