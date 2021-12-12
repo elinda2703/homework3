1ed7b74b9207c4fc25dbd9eaf2ca903723a137eb
elinda <ela.pospechova@gmail.com>
2021-12-12 21:21:28 +0100

    elegantnější ukončení programu, když jsou minimální vzdálenosti moc velké

--- du3.py
+++ du3.py
@@ -12,27 +12,31 @@ try:
 
     wgs2jtsk = Transformer.from_crs(4326, 5514)
     vzdalenosti_min = {}
+    adresy_kontejnery={}
     vzdalenost = None
     for i in range(len(adresy['features'])):
         ulice_cislo = f"{adresy['features'][i]['properties']['addr:street']} {adresy['features'][i]['properties']['addr:housenumber']}"
         adresa_y, adresa_x = adresy['features'][i]['geometry']['coordinates']
-        souradnice_krovak = wgs2jtsk.transform(adresa_x, adresa_y)
+        krovak_x, krovak_y = wgs2jtsk.transform(adresa_x, adresa_y)
         vzdalenosti_min[ulice_cislo] = None
         for j in range(len(kontejnery['features'])):
+            id_kontejneru=kontejnery['features'][j]['properties']['ID']
             adresa_kontejneru = str(
                 kontejnery['features'][j]['properties']['STATIONNAME'])
             kontejner_x, kontejner_y = kontejnery['features'][j]['geometry']['coordinates']
             vzdalenost = int(round(sqrt(
-                (souradnice_krovak[0]-kontejner_x)**2+(souradnice_krovak[1]-kontejner_y)**2), 0))
+                (krovak_x-kontejner_x)**2+(krovak_y-kontejner_y)**2), 0))
             if kontejnery['features'][j]['properties']['PRISTUP'] == "obyvatelům domu" and ulice_cislo == adresa_kontejneru:
                 vzdalenosti_min[ulice_cislo] = 0
-            elif kontejnery['features'][j]['properties']['PRISTUP'] == "volně":
+                break
+            if kontejnery['features'][j]['properties']['PRISTUP'] == "volně":
                 if vzdalenosti_min[ulice_cislo] == None or vzdalenosti_min[ulice_cislo] > vzdalenost:
                     vzdalenosti_min[ulice_cislo] = vzdalenost
-        if vzdalenosti_min[ulice_cislo] > 10000:
-            print("příliš velká vzdálenost k nejblišímu kontejneru")
-            sys.exit()
+                    adresy_kontejnery[ulice_cislo]=id_kontejneru
 
+        if vzdalenosti_min[ulice_cislo] > 10000:
+            raise Exception
+   
     maximum = 0
     adresa_maxima = None
     median = statistics.median(vzdalenosti_min.values())
@@ -45,6 +49,11 @@ try:
     print(f"Průměrná vzdálenost k nejbližšímu kontejneru je {prumer} m.")
     print(f"Nejdále je to ke kontejneru z adresy {adresa_maxima} a to {maximum} m.")
     print(f"Medián vzdáleností je {median} m.")
-    print(i, j)
+    print(len(adresy_kontejnery))
+    
+    
+
 except FileNotFoundError:
-    print("chybná vstupní data")
+    print("chybná vstupní data nebo špatný adresář")
+except Exception:
+    print("podezřele velké vzdálenosti k nejbližším kontejnerům")
