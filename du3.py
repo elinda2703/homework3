c4d632a4f0916d2183a1ecf3357e9b35c7068469
elinda <ela.pospechova@gmail.com>
2021-12-17 03:16:57 +0100

    parsing, komentáře

--- du3.py
+++ du3.py
@@ -1,10 +1,10 @@
 import json
 from pyproj import Transformer
-from math import sqrt
+from math import sqrt, inf
 import statistics
 import sys
 from geojson import dump
-
+# načte data ze souborů s adresními body a kontejnery, spadne, když nemůže soubor najít nebo ho program nemá právo číst
 try:
     with open("adresy.geojson", encoding="utf-8") as a:
         adresy = json.load(a)
@@ -13,49 +13,94 @@ try:
         kontejnery = json.load(k)
 except FileNotFoundError:
     sys.exit("chybná vstupní data nebo špatný adresář")
+except PermissionError:
+    sys.exit("program nemá právo číst vstupní soubor")
 
-def pythagorova_veta (x1, x2, y1, y2):
+# spočítá délku přepony pravoúhlého trojúhelníku při zadaných souřadnicích koncových bodů této přepony
+
+
+def pythagorova_veta(x1, x2, y1, y2):
     prepona = sqrt((x1-x2)**2+(y1-y2)**2)
     return prepona
-        
-wgs2jtsk = Transformer.from_crs(4326, 5514, always_xy = True)
-vzdalenosti_min = {}
-adresy_kontejnery={}
-vzdalenost = None
-maximum = 0
-adresa_maxima = None
-for i in range(len(adresy['features'])):
-    ulice_cislo = f"{adresy['features'][i]['properties']['addr:street']} {adresy['features'][i]['properties']['addr:housenumber']}"
-    adresa_x, adresa_y = adresy['features'][i]['geometry']['coordinates']
-    krovak_x, krovak_y = wgs2jtsk.transform(adresa_x, adresa_y)
-    vzdalenosti_min[ulice_cislo] = float('inf')
 
-    for j in range(len(kontejnery['features'])):
-        id_kontejneru=kontejnery['features'][j]['properties']['ID']
-        adresa_kontejneru = str(
-            kontejnery['features'][j]['properties']['STATIONNAME'])
-        kontejner_x, kontejner_y = kontejnery['features'][j]['geometry']['coordinates']
-        if kontejnery['features'][j]['properties']['PRISTUP'] == "obyvatelům domu" and ulice_cislo == adresa_kontejneru:
-            vzdalenosti_min[ulice_cislo] = 0  
-            adresy['features'][i]['properties']['id_nejblizsiho_kontejneru']=id_kontejneru
-        if kontejnery['features'][j]['properties']['PRISTUP'] == "volně":
-            vzdalenost=pythagorova_veta(kontejner_x,krovak_x,kontejner_y,krovak_y)
+
+# převede WGS souřadnice na S-JTSK souřadnice
+wgs2jtsk = Transformer.from_crs(4326, 5514, always_xy=True)
+# do tohoto slovníku se budou ukládat adresy (klíč) a vzdálenost k nejbližšímu kontejneru (hodnota)
+vzdalenosti_min = {}
+# sem se budou ukládat adresy (klíč) a ID nejbližšího kontejneru (hodnota)
+adresa_id_kontejneru = {}
+
+MAX_VZDALENOST = 10000  # maximální vzdálenost nejbližšího kontejneru
+
+
+# získá potřebné informace ze souboru s adresami
+def parse_addresa(adresa):
+    ulice_cislo = f"{adresa['properties']['addr:street']} {adresa['properties']['addr:housenumber']}"
+    adresa_x, adresa_y = adresa['geometry']['coordinates']
+    id_adresy=adresa['properties']['@id']
+    return adresa_x, adresa_y, ulice_cislo, id_adresy
+
+# získá potřebné informace ze souboru s kontejnery
+
+
+def parse_kontejner(kontejner):
+    id_kontejneru = kontejner['properties']['ID']
+    adresa_kontejneru = str(
+        kontejner['properties']['STATIONNAME'])
+    kontejner_x, kontejner_y = kontejner['geometry']['coordinates']
+    pristup = kontejner['properties']['PRISTUP']
+    return adresa_kontejneru, id_kontejneru, kontejner_x, kontejner_y, pristup
+
+
+# uloží potřebné informace o adresách do seznamu n-tic
+parsovane_adresy = [parse_addresa(x) for x in adresy['features']]
+# uloží potřebné informace o kontejnerech do seznamu n-tic
+parsovane_kontejnery = [parse_kontejner(x) for x in kontejnery['features']]
+
+# v tomto cyklu se převedou sořadnice adresních bodů do S-JTSK a vzdálenost k nejbližšímu kontejneru je inicializovůna jako nekonečno
+for adresa_x, adresa_y, ulice_cislo, id_adresy in parsovane_adresy:
+    krovak_x, krovak_y = wgs2jtsk.transform(adresa_x, adresa_y)
+    vzdalenosti_min[ulice_cislo] = inf
+    # v tomto cyklu se adresám s privátním kontejnerem (pristup == "obyvatelům domu") přiřadí nulová vzdálenost k nejbližšímu kontejneru a těm ostatním se přiřadí nejmenší vypočítaná vzdálenost ke kontejneru
+
+    for adresa_kontejneru, id_kontejneru, kontejner_x, kontejner_y, pristup in parsovane_kontejnery:
+        if pristup == "obyvatelům domu" and ulice_cislo == adresa_kontejneru:
+            vzdalenosti_min[ulice_cislo] = 0
+          
+            adresa_id_kontejneru[id_adresy] = id_kontejneru
+        if pristup == "volně":
+            vzdalenost = pythagorova_veta(
+                kontejner_x, krovak_x, kontejner_y, krovak_y)
             if vzdalenosti_min[ulice_cislo] > vzdalenost:
                 vzdalenosti_min[ulice_cislo] = vzdalenost
-                adresy_kontejnery[ulice_cislo]=id_kontejneru
-                adresy['features'][i]['properties']['kontejner']=id_kontejneru
+                
+                adresa_id_kontejneru[id_adresy] = id_kontejneru
+    # program spadne, když jsou vzdálenosti k nejbližším kontejnerů příliš velké
+    if vzdalenosti_min[ulice_cislo] > MAX_VZDALENOST:
+        sys.exit("podezřele velké vzdálenosti k nejbližším kontejnerům")
 
-if vzdalenosti_min[ulice_cislo] > 10000:
-    sys.exit("podezřele velké vzdálenosti k nejbližším kontejnerům")
-        
-median = int(round(statistics.median(vzdalenosti_min.values()),0))
-prumer = int(round(statistics.mean(vzdalenosti_min.values()), 0))
+# výpočet mediánu vzdáleností k nejbližšímu kontejneru
+median = round(statistics.median(vzdalenosti_min.values()))
+# výpočet průměru vzdáleností k nejbližšímu kontejneru
+prumer = round(statistics.mean(vzdalenosti_min.values()))
+
+for i in adresy['features']:
+    for k,v in adresa_id_kontejneru.items():
+        if k==i['properties']['@id']:
+            i['properties']['kontejner']=v
+
+maximum = -inf
+# získání největší vzdálenosti k nejbližšímu kontejneru
 for a, d in vzdalenosti_min.items():
     if maximum < d:
         maximum = d
         adresa_maxima = str(a)
+
+# výpis
 print(f"Průměrná vzdálenost k nejbližšímu kontejneru je {prumer} m.")
-print(f"Nejdále je to ke kontejneru z adresy {adresa_maxima} a to {int(round(maximum))} m.")
+print(
+    f"Nejdále je to ke kontejneru z adresy {adresa_maxima} a to {round(maximum)} m.")
 print(f"Medián vzdáleností je {median} m.")
 with open('adresy_kontejnery.geojson', 'w', encoding="utf-8") as f:
     dump(adresy, f, ensure_ascii=False, indent=4)
